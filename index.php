5a9c1b138c3983bc0a04c7c1ace3d4802b84c518
vikas <vikyrcok499>
2018-03-07 18:53:33 +0530

    commited by pankaj

--- index.php
+++ index.php
@@ -8,9 +8,13 @@
 			<h1>Testing </h1>
 			<?php echo "LEARN GIT HUB"; ?>
 			<?php echo "TELL ME HOW??"; ?>
+<<<<<<< Updated upstream
 			<h1> new test </h1>
 			<p>username : vikas</p>
 			<P>Password  : 12345</p>
+=======
+			<h1>ahshasdh asjsa aksjkdk mfasjhkas naskkfj nakjsnfk nkjkjasfd mbkaf mbkasf kbaibfa mbaisbfkajsbam jbkuasbfkjs bisuafbfisusf kbfisbfkjm mn jabfbk jbifak kbifabkjasf mbiufsa kiuabi afsuifias  iasfofas </h1>
+>>>>>>> Stashed changes
 		</div>
 	</body>
 
